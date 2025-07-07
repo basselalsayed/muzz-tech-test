0f77ab837a8ea5bf59fea45d6000f4479d40b3a4
Bassel Al-Sayed <53341220+basselalsayed@users.noreply.github.com>
2025-07-07 11:33:05 +0100

    user is typing indicator test

--- apps/frontend/e2e/chat.spec.ts
+++ apps/frontend/e2e/chat.spec.ts
@@ -1,6 +1,41 @@
-import { expect, test } from '@playwright/test';
+import { type BrowserContext, type Page, expect, test } from '@playwright/test';
 
-test('test', async ({ context }) => {
+test('chat between users', async ({ context }) => {
+  const [userOneChat, userTwoChat] = await setupTwoUsers(context);
+
+  await userOneChat
+    .getByRole('textbox', { name: 'Message John' })
+    .fill('Hello John');
+  await userOneChat.getByRole('button', { name: 'Send' }).click();
+
+  await userTwoChat
+    .getByRole('textbox', { name: 'Message Alisha' })
+    .fill('Hello Alisha');
+  await userTwoChat.getByRole('button', { name: 'Send' }).click();
+
+  await expect(userTwoChat.getByText('Hello John')).toBeVisible();
+  await expect(userOneChat.getByText('Hello Alisha')).toBeVisible();
+});
+
+test('typing indicator test', async ({ context }) => {
+  const [userOneChat, userTwoChat] = await setupTwoUsers(context);
+
+  const textbox = userOneChat.getByRole('textbox', { name: 'Message John' });
+  await textbox.click();
+
+  const longMessage =
+    'This is a very long message that should trigger the typing indicator. '.repeat(
+      3
+    );
+
+  await textbox.pressSequentially(longMessage, { delay: 100 });
+
+  await expect(userTwoChat.getByText('Alisha is typing...')).toBeVisible();
+
+  await expect(userTwoChat.getByText('Alisha is typing...')).not.toBeVisible();
+});
+
+async function setupTwoUsers(context: BrowserContext): Promise<[Page, Page]> {
   const userOneChat = await context.newPage();
   const userTwoChat = await context.newPage();
 
@@ -25,16 +60,5 @@ test('test', async ({ context }) => {
     .getByRole('button')
     .click();
 
-  await userOneChat
-    .getByRole('textbox', { name: 'Message John' })
-    .fill('Hello John');
-  await userOneChat.getByRole('button', { name: 'Send' }).click();
-
-  await userTwoChat
-    .getByRole('textbox', { name: 'Message Alisha' })
-    .fill('Hello Alisha');
-  await userTwoChat.getByRole('button', { name: 'Send' }).click();
-
-  await expect(userTwoChat.getByText('Hello John')).toBeVisible();
-  await expect(userOneChat.getByText('Hello Alisha')).toBeVisible();
-});
+  return [userOneChat, userTwoChat];
+}
